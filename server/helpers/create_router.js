cdc1e69ab0fc2e8cb725e82a8236a2b718344f91
Kyle Johnston <kyle9657@gmail.com>
2019-02-27 11:53:10 +0000

    Add router to insert new data into database

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -29,6 +29,18 @@ const createRouter = function (collection) {
     });
   });
 
+  router.post('/', (req, res) => {
+    const newBooking = req.body;
+    collection
+    .insertOne(newBooking)
+    .then(result => res.json(result.ops[0]))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
 
 
 
