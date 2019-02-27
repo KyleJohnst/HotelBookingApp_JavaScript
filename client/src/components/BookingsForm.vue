d0ff3a000be783f8775e7e7fbf13d15b6f72c5fd
Kyle Johnston <kyle9657@gmail.com>
2019-02-27 14:19:09 +0000

    Completed method to post to database from submit

--- client/src/components/BookingsForm.vue
+++ client/src/components/BookingsForm.vue
@@ -1,31 +1,58 @@
 <template lang="html">
-<form id="" action="index.html" method="post" v-on:submit="addBooking">
-  <h2>Create booking</h2>
-  <div class="formStyle">
-    <label for="name">Name:</label>
-    <input type="text" >
-  </div>
+  <form id="" action="index.html" method="post" v-on:submit="addBooking">
+    <h2>Create booking</h2>
+    <div class="formStyle">
+      <label for="name">Name:</label>
+      <input type="text" v-model="name"/>
+    </div>
 
-  <div class="formStyle">
-    <label for="surname">Surname:</label>
-    <input type="text" >
-  </div>
+    <div class="formStyle">
+      <label for="surname">Surname:</label>
+      <input type="text" v-model="surname"/>
+    </div>
 
-  <div class="formStyle">
-    <label for="email">Email</label>
-    <input type="text" >
-  </div>
+    <div class="formStyle">
+      <label for="email">Email</label>
+      <input type="text" v-model="email"/>
+    </div>
 
-  <div class="formStyle">
+    <!-- <div class="formStyle">
     <label for="checkedIn">Checked In</label>
     <input type="checkbox" >
-  </div>
-
+  </div> -->
+  <input type="submit" value="Save" id="save"/>
 </form>
 </template>
 
 <script>
+import {eventBus} from '../main';
+
 export default {
+  name: 'BookingsForm',
+  data(){
+    return {
+      name: '',
+      surname: '',
+      email: '',
+      checkedIn: false
+    }
+  },
+  methods: {
+    addBooking(e){
+      e.preventDefault()
+      const booking = {
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        checkedIn: this.checkedIn
+      }
+      fetch('http://localhost:3000/api/bookings', {
+        method: 'POST',
+        body: JSON.stringify(booking),
+        headers: { 'Content-Type': 'application/json'}
+      })
+    }
+  }
 }
 </script>
 
